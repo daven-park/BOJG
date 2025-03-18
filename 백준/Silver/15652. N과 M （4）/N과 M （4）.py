73bf9a84071b33c613221260df5b07dd13cfe903
ChangHyun Park <buschanny@gmail.com>
2025-03-18 23:15:06 +0900

    [Silver III] Title: N과 M (4), Time: 132 ms, Memory: 111636 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,18 @@
+import sys
+
+N, M = map(int, input().split())
+
+arr = []
+
+isUsed = [False] * 10
+def func(k : int, start : int):
+    if k == M:
+        print(" ".join(map(str, arr[:M])))
+        return
+
+    for i in range(start, N + 1):
+            arr.append(i)
+            func(k + 1, i)
+            arr.pop()
+
+func(0, 1)
