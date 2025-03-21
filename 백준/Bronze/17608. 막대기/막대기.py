388df20108b20346650e72be8c12c7b1893d118e
ChangHyun Park <buschanny@gmail.com>
2025-03-21 19:53:42 +0900

    [Bronze II] Title: 막대기, Time: 140 ms, Memory: 117744 KB -BaekjoonHub

--- 백준/Bronze/17608. 막대기/막대기.py
+++ 백준/Bronze/17608. 막대기/막대기.py
@@ -1,14 +1,12 @@
 N = int(input())
 
 stack = []
-numbers = []
-for i in range(N):
-    num = int(input())
-    numbers.append(num)
+numbers = [int(input()) for num in range(N)]
 
 stack.append(numbers[len(numbers) - 1])
 for number in reversed(numbers):
     if number > stack[len(stack) - 1]:
         stack.append(number)
+
 
 print(len(stack))
