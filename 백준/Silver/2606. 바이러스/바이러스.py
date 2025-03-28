73bbed698b72be93759b20a559bdc6cbb02c0ac6
ChangHyun Park <buschanny@gmail.com>
2025-03-29 01:52:42 +0900

    [Silver III] Title: 바이러스, Time: 88 ms, Memory: 109412 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,34 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+M = int(input())
+
+parent = [0] * (N + 1)
+
+for i in range(1, N + 1):
+    parent[i] = i
+
+def find(a):
+    if a == parent[a]:
+        return a
+    else:
+        parent[a] = find(parent[a])
+        return parent[a]
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    if a != b:
+        parent[b] = a
+
+for _ in range(M):
+    s, e = map(int, input().split())
+    if find(s) != find(e):
+        union(s, e)
+
+for i in range(1, N + 1):
+    find(i)
+
+print(sum(1 for i in range(2, N + 1) if find(i) == find(1)))
