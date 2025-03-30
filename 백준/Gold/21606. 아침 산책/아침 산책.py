6dcb637c3ffbd64c0179bce5c5fbb2b24e2128bf
ChangHyun Park <buschanny@gmail.com>
2025-03-30 23:27:23 +0900

    [Gold III] Title: 아침 산책, Time: 980 ms, Memory: 79696 KB, Score: 3 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,5 +1,6 @@
 import sys
 from collections import defaultdict
+sys.setrecursionlimit(10**9)
 
 input = sys.stdin.readline
 
@@ -15,34 +16,31 @@ N = int(input())
 
 graph = defaultdict(list)
 place = [int(char) for char in "0" + input().strip()]
+count = 0
 for i in range(N - 1):
     s, e = map(int, input().split())
     graph[s].append(e)
     graph[e].append(s)
-count = 0
-visited = [0] * (N + 1)
+    if place[s] == 1 and place[e] == 1:    # 실내끼리 붙어있다면 카운트 체크
+        count += 2
 
-def dfs_outside(node): # 사이에 실외가 있는 구간 카운트
+
+visited = [0] * (N + 1)
+def dfs_outside(node, inside_count): # 사이에 실외가 있는 구간 카운트
     visited[node] = True
-    inside_count = 0
 
     for neighbor in graph[node]:
-        if place[neighbor] == 1:
+        if place[neighbor] == 1:    # 실내일때
             inside_count += 1
-        elif not visited[neighbor]:
-            inside_count += dfs_outside(neighbor)
+        elif not visited[neighbor] and place[neighbor] == 0:   # 실외이고 아직 방문하지 않았을 때
+            inside_count += dfs_outside(neighbor, inside_count) # dfs
 
     return inside_count
 
-
+sum_count = 0
 for i in range(1, N + 1):
-    if place[i] == 0 and not visited[i]:
-        k = dfs_outside(i)
-        count += k * (k - 1)
+    if place[i] == 0 and not visited[i]:    # 실외인 노드를 기준으로 dfs
+        k = dfs_outside(i, 0)
+        sum_count += k * (k - 1)
 
-for i in range(1, N + 1):
-    if place[i] == 1:
-        for neighbor in graph[i]:
-            if place[neighbor] == 1:
-                count += 1
-print(count)
+print(count + sum_count)
