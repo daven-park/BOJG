86b0ba2b3e1fd54c63a3ea65db167e61c126fbcd
ChangHyun Park <buschanny@gmail.com>
2025-04-02 02:07:03 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 236 ms, Memory: 123808 KB -BaekjoonHub

---
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -0,0 +1,40 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+maps = [[int(char) for char in input().split()] for _ in range(N)]
+
+start = None
+for i in range(N):
+    for j in range(M):
+        if maps[i][j] == 2:
+            start = (i, j)
+visited = [[False] * M for _ in range(N)]
+ans = [[-1] * M for _ in range(N)]
+def bfs():
+    queue = deque([(start[0], start[1], 0)])
+    visited[start[0]][start[1]] = True
+    ans[start[0]][start[1]] = 0
+    while queue:
+        curX, curY, dist = queue.popleft()
+        for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            nx = dx + curX
+            ny = dy + curY
+            if nx < 0 or ny < 0 or nx >= N or ny >= M: continue
+            if ans[nx][ny] != -1: continue
+            if maps[nx][ny] != 1: continue
+            queue.append((nx, ny, dist + 1))
+            ans[nx][ny] = dist + 1
+
+bfs()
+
+for i in range(N):
+    for j in range(M):
+        if maps[i][j] == 0:
+            print(0, end =' ')
+        else:
+            print(ans[i][j], end = ' ')
+    print()
