534c10cb6b5035b6068fa4ac405c5391d646fcc2
ChangHyun Park <buschanny@gmail.com>
2025-05-07 13:43:23 +0900

    [Gold V] Title: 전생했더니 슬라임 연구자였던 건에 대하여 (Easy), Time: 2520 ms, Memory: 131676 KB -BaekjoonHub

--- 백준/Gold/14715. 전생했더니 슬라임 연구자였던 건에 대하여 （Easy）/전생했더니 슬라임 연구자였던 건에 대하여 （Easy）.py
+++ 백준/Gold/14715. 전생했더니 슬라임 연구자였던 건에 대하여 （Easy）/전생했더니 슬라임 연구자였던 건에 대하여 （Easy）.py
@@ -3,24 +3,32 @@ import sys
 
 input = sys.stdin.readline
 
-K = int(input())
+# 슬라임 에너지는 2의 자연수 2 <= K <= 1000000
 
-# 소수 판별 배열: False → 소수, True → 소수 아님
+# K = A * B
+K = int(input())
+# 분할할때마다 흠집 + 1
+# 흠집이 T개인 슬라임 분할하면 흠집 T + 1
+
+# 에라토스테네스의 체를 사용해서 소수 배열 만들것.
 prime = [False] * 1000001
 for i in range(2, 1001):
-    if prime[i]:
-        continue
+    if prime[i]: continue
     for j in range(i * i, 1000001, i):
         prime[j] = True
 
-def recursive(N):
-    if N < 2 or not prime[N]:  # 예외 처리 추가
+
+def dfs(N):
+    # 소수까지 도착했다면
+    if N < 2 or not prime[N]:
         return 0
-    answer = float('inf')
+    min_value = 10 ** 9
     for i in range(2, int(math.sqrt(N)) + 1):
         if N % i == 0:
-            depth = 1 + max(recursive(i), recursive(N // i))
-            answer = min(answer, depth)
-    return answer
+            # 소수와 소수로 나뉘어진 값으로 나누기
+            depth = 1 + max(dfs(i), dfs(N // i))
+            min_value = min(min_value, depth)
+    return min_value
 
-print(recursive(K))
+
+print(dfs(K))
