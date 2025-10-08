54660a2b16fe046ed599901e4ed50bfec91f6e5e
ChangHyun Park <buschanny@gmail.com>
2025-10-08 14:32:12 +0900

    [Gold IV] Title: LCS 2, Time: 120 ms, Memory: 118452 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -0,0 +1,28 @@
+import sys
+
+A = input().strip()
+B = input().strip()
+
+dp = [[0] * (len(B) + 1) for _ in range(len(A) + 1)]
+
+for i in range(1, len(A) + 1):
+    for j in range(1, len(B) + 1):
+        if A[i - 1] == B[j - 1]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+i, j = len(A), len(B)
+lcs = []
+while i > 0 and j > 0 :
+    if A[i - 1] == B[j - 1]:
+        lcs.append(A[i - 1])
+        i -= 1
+        j -= 1
+    elif dp[i - 1][j] > dp[i][j - 1]:
+        i -= 1
+    else:
+        j -= 1
+
+print(dp[len(A)][len(B)])
+print(''.join(reversed(lcs)))
