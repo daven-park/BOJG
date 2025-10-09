7eece8b4703b724214aecc587b610160c9b67ac1
ChangHyun Park <buschanny@gmail.com>
2025-10-10 00:07:27 +0900

    [Gold III] Title: 최소 편집, Time: 128 ms, Memory: 118632 KB -BaekjoonHub

---
+++ 백준/Gold/15483. 최소 편집/최소 편집.py
@@ -0,0 +1,22 @@
+import sys
+
+A = input().strip()
+B = input().strip()
+
+dp = [[0] * (len(B) + 1) for _ in range(len(A) + 1)]
+
+for i in range(len(A) + 1):
+    dp[i][0] = i
+for i in range(len(B) + 1):
+    dp[0][i] = i
+
+for i in range(1, len(A) + 1):
+    for j in range(1, len(B) + 1):
+        if A[i - 1] == B[j - 1]:
+            dp[i][j] = dp[i - 1][j - 1]
+        else:
+            dp[i][j] = min(dp[i][j - 1] + 1,
+                           dp[i - 1][j] + 1,
+                           dp[i - 1][j - 1] + 1)
+
+print(dp[len(A)][len(B)])
